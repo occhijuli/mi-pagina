678efb03a3c13c13622cf431f6defe3857b32cf1
Occhi890 <julianocchipinti2004@gmail.com>
2024-09-03 11:21:44 -0300

    agrego los archivos xd

---
+++ main.js
@@ -0,0 +1,10 @@
+//crear una funcion que se ejecutara 
+function muestra_oculta(id){
+    let div = document.getElementById(id);
+    if (div.style.display == "none"){
+        div.style.display = "flex";
+    }
+    else{
+        div.style.display = "none";
+    }
+}
